4aea45866d8bea8f3635b0198d4fea1ed504790f
Licy <mtslicy@126.com>
2016-06-02 00:15:56 +0800

    secend time

---
+++ src/com/db/beans/TextAndAudio.java
@@ -0,0 +1,35 @@
+package com.db.beans;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class TextAndAudio {
+	private int id;
+	private String text;
+	private String audio;
+	
+	public TextAndAudio(){}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getAudio() {
+		return audio;
+	}
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
+	
+	
+}
